425ed35f870e7fd7cc9e4eca061feea98c4b4461
cloudbyteelias <cloudbyteelias@gmail.com>
2021-02-04 20:43:46 -0300

    Warning level

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
 
+;(setq warning-minimum-level :emergency)
+
+
+(eval-after-load "enriched"
+  '(defun enriched-decode-display-prop (start end &optional param)
+     (list start end)))
+
 (defun load-directory (dir)
       (let ((load-it (lambda (f)
 		       (load-file (concat (file-name-as-directory dir) f)))
@@ -8,8 +15,14 @@
 
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(package-selected-packages '(no-littering use-package)))
 (custom-set-faces
-
-
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
